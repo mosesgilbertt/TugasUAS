7f545235a4eae77d324f266211d07131b4795783
Moses Gilbert <mosesgilbert4@gmail.com>
2021-06-24 13:43:41 +0700

    Merge branch 'main' of https://github.com/mosesgilbertt/TugasUAS

---
+++ views/About.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="about">
+    <table>
+      <tr>
+        <td>
+          Country
+        </td>
+        <td>
+          Confirmed
+        </td>
+        <td>
+          Deaths
+        </td>
+        <td>
+          Recovered
+        </td>
+      </tr>
+      <tr v-for="isi in lst" v-bind:key="isi.id">
+        <td>
+            {{isi.Country}}
+          </td>
+          <td>
+            {{isi.TotalConfirmed}}
+          </td>
+          <td>
+            {{isi.TotalDeaths}}
+          </td>
+          <td>
+            {{isi.TotalRecovered}}
+          </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'About',
+  data() {
+    return {lst:['Countries']}
+  },
+  mounted()
+  {
+    axios.get('https://api.covid19api.com/summary').then((resp) => {
+      this.list=resp.data;
+      console.warn(resp.data)
+      })
+      }
+}
+</script>
