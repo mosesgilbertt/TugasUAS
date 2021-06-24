fcdd31af75926efdd1ac5f49e43bcb29eafd28c2
Moses Gilbert <mosesgilbert4@gmail.com>
2021-06-24 13:29:18 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Data Global</router-link> |
+      <router-link to="/about">Data Negara</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div  v-for="item in list" class="flex space-x-4">
+      <div class="flex-1 bg-blue-200 border-4">
+      Total Confirmed <br>
+      {{item.TotalConfirmed}}
+      </div>
+      <div class="flex-1 ...">
+      Total Deaths <br>
+      {{item.TotalDeaths}}
+      </div>
+      <div class="flex-1 ...">
+      Total Recovered <br>
+      {{item.TotalRecovered}}
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'App',
+  data() {
+    return {list:['Global']}
+  },
+  mounted()
+  {
+    axios.get('https://api.covid19api.com/summary').then((resp) => {
+      this.list=resp.data;
+      console.warn(resp.data)
+      })
+      }
 }
 </script>
